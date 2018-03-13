7742f7c87eb590236558fabed894c289fdd3b4c5
ongfei <ong_fei@163.com>
2018-03-13 15:13:51 +0800

    ss

--- DFAnimatedTransition/DFAnimatedTransition/DFAnimatedTransition/DFPercentDrivenInteractiveTransition.h
+++ DFAnimatedTransition/DFAnimatedTransition/DFAnimatedTransition/DFPercentDrivenInteractiveTransition.h
@@ -7,9 +7,24 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "DFTransitionConfigType.h"
+
+typedef void(^ActionBlock)(void);
 
 //主要用来处理手势
-
 @interface DFPercentDrivenInteractiveTransition : UIPercentDrivenInteractiveTransition
+
+@property (nonatomic, assign) DFGestureType getstureType;
+@property (readonly, assign, nonatomic) BOOL isInteractive;
+@property (nonatomic, assign) DFTransitionType transitionType;
+
+@property (nonatomic, copy) ActionBlock presentBlock;
+@property (nonatomic, copy) ActionBlock pushBlock;
+@property (nonatomic, copy) ActionBlock dismissBlock;
+@property (nonatomic, copy) ActionBlock popBlock;
+
+@property (nonatomic, copy) void(^willEndInteractiveBlock)(BOOL success);
+
+- (void)addGestureToViewController:(UIViewController *)vc;
 
 @end
